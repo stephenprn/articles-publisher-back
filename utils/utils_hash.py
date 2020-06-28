61a23955f7cf9e37889e52cd459a3ef8b5e6bb24
Stephen <stephen@nr2.io>
2020-06-28 20:32:07 +0200

    log check_pw

--- utils/utils_hash.py
+++ utils/utils_hash.py
@@ -20,38 +20,11 @@ def hash_password(password: str):
 
 
 def check_password(pw_input: str, salt: str, pw_hashed: str):
-    print("pw_input")
-    print(pw_input)
-    print("pw_hashed")
-    print(pw_hashed)
-    try:
-        a = pw_input.encode('utf-8')
-    except Exception as e:
-        print("aaaa")
-        print(e)
-    try:
-        pw_input_hashed = hashlib.pbkdf2_hmac(
-            'sha256',
-            a,
-            salt.encode('utf-8'),
-            100000
-        )
-    except Exception as e:
-        print("bbbb")
-        print(e)
-
-    try:
-        pw_input_hashed = b64encode(pw_input_hashed)
-    except Exception as e:
-        print("ccccc")
-        print(e)
     pw_input_hashed = b64encode(hashlib.pbkdf2_hmac(
         'sha256',
         pw_input.encode('utf-8'),  # Convert the password to bytes
-        salt,
+        salt.encode('utf-8'),
         100000
     ))
-    print("pw_input_hashed")
-    print(pw_input_hashed)
 
     return pw_hashed == pw_input_hashed
