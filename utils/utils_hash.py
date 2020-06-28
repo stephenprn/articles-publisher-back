4a307236224604567ce5c140db9d34a1b9e368fb
Stephen <stephen@nr2.io>
2020-06-28 20:50:38 +0200

    log check_pw

--- utils/utils_hash.py
+++ utils/utils_hash.py
@@ -8,24 +8,24 @@ def generate_random_salt():
 
 
 def hash_password(password: str):
-    salt = generate_random_salt()
+    salt = str(b64encode(generate_random_salt()))
 
-    password_hashed = b64encode(hashlib.pbkdf2_hmac(
+    password_hashed = str(b64encode(hashlib.pbkdf2_hmac(
         'sha256',
         password.encode('utf-8'),
         salt.encode('utf-8'),
         100000
-    ))
+    )))
 
     return (password_hashed, salt)
 
 
 def check_password(pw_input: str, salt: str, pw_hashed: str):
-    pw_input_hashed = b64encode(hashlib.pbkdf2_hmac(
+    pw_input_hashed = str(b64encode(hashlib.pbkdf2_hmac(
         'sha256',
         pw_input.encode('utf-8'),  # Convert the password to bytes
         salt.encode('utf-8'),
         100000
-    ))
+    )))
 
     return pw_hashed == pw_input_hashed
